982e9a2478647fa318d679c11276299104a60f03
Prashant Sharma <87496363+prashantGit2@users.noreply.github.com>
2023-05-29 13:00:27 +0530

    swr implemented

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,7 +2,9 @@
 
 import {useState,useEffect} from "react"
 import PromptCard from "./PromptCard";
-
+import useSWR from 'swr';
+ 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const PromptCardList = ({data,handleTagClick}) => {
   return (
     <div className="mt-16 prompt_layout">
@@ -20,20 +22,10 @@ const PromptCardList = ({data,handleTagClick}) => {
 
 const Feed = () => {
   const [searchText,setSearchText] = useState("");
-  const [posts,setPosts] = useState([]);
+  const { data:posts, error } = useSWR('/api/prompt/getAllPrompts', fetcher);
   const handleChangeSearch = (e) => {
     setSearchText(e.target.value);
   }
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const resp = await getServerSideProps();
-      setPosts(resp);
-    }
-    fetchPosts();
-  }, [])
-  
-
   return (
     <section className="feed">
       <form 
@@ -54,10 +46,16 @@ const Feed = () => {
     </section>
   )
 }
-export const getServerSideProps = async () => {
-  const response = await fetch("/api/prompt/getAllPrompts");
-  const data = await response.json();
-  return data;
-}
+// export async function getStaticProps({}){
+//   const response = await fetch("/api/prompt/getAllPrompts");
+//   console.log("this ran")
+//   const data = await response.json();
+//   return {
+//     props: {
+//       posts: data
+//     },
+//     revalidate: 1,
+//   }
+// }
 
 export default Feed
